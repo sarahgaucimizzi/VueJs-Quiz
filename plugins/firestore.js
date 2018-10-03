ea58960fc2ff85c45d78802907440273672288d7
Sarah Mizzi <sar.mizzi@gmail.com>
2018-10-03 08:39:04 +0200

    Update firestore.js

--- plugins/firestore.js
+++ plugins/firestore.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyA4HvwXuqQT7cly-Tn4tUZoZzY_T0Zwzgo',
+    authDomain: 'quiz-46f2c.firebaseapp.com',
+    databaseURL: 'https://quiz-46f2c.firebaseio.com',
+    projectId: 'quiz-46f2c',
+    storageBucket: 'quiz-46f2c.appspot.com',
+    messagingSenderId: '773540484667'
   })
 }
 
